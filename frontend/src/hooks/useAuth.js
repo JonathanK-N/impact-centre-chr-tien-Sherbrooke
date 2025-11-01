f0a4861350145f5694620d3e541dbaca9ed91995
Jonathan K-N <jkakesa9@gmail.com>
2025-11-01 13:53:10 -0400

    sup

--- frontend/src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-import { useAuthContext } from "../context/AuthContext";
-
-export default function useAuth() {
-  const context = useAuthContext();
-  if (!context) {
-    throw new Error("useAuth must be used within AuthProvider");
-  }
-  return context;
-}
