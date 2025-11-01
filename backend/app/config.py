a6bf66ceaa2846a97392a881340847961dff58b6
Jonathan K-N <jkakesa9@gmail.com>
2025-11-01 12:25:43 -0400

    fix

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,7 @@ import os
 from datetime import timedelta
 
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-INSTANCE_DIR = os.path.join(BASE_DIR, "..", "instance")
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
 os.makedirs(INSTANCE_DIR, exist_ok=True)
 
 
